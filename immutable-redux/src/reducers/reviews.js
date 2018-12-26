41506488624fadf8901760982e589a1793f4af15
Tien Pham <r3dsky02@yahoo.com.vn>
2018-12-26 23:09:51 +0200

    [immutable] Green!

---
+++ immutable-redux/src/reducers/reviews.js
@@ -0,0 +1,28 @@
+import { List, Map } from 'Immutable';
+import types from '../actionTypes';
+
+const reviews = (state=List(), action) => {
+  switch (action.type) {
+    case types.reviews.ADD_REVIEW:
+      const newReview = Map(
+        { id: action.id,
+          item_id: action.item_id,
+          reviewer: action.reviewer,
+          text: action.text,
+          rating: action.rating,
+          flag: false
+        }
+      )
+      return state.push(newReview);  // Note that Immutable's push will return a new array
+    case types.reviews.DELETE_REVIEW:
+      return state.filter(review => review.id !== action.id);
+    case types.reviews.FLAG_REVIEW:
+      return state.map(review => review.id === action.id ? Object.assign({}, review, { flag: action.flag}): review)
+    case types.reviews.RATE_REVIEW:
+      return state.map(review => review.id === action.id ? {...review, rating: action.rating }: review)
+    default:
+      return state;
+  }
+}
+
+export default reviews;
